b4839715cffd53eb87140a26609086164b12f164
Caner Derici <caner@dericilab.live>
2025-09-22 13:16:27 -0600

    fix(cname): add cname into the src

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,7 @@ export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/assets/img": "assets/img" });
   eleventyConfig.addPassthroughCopy({ "src/assets/audio": "assets/audio" });
   eleventyConfig.addPassthroughCopy({ "src/assets/docs": "assets/docs" });
+  eleventyConfig.addPassthroughCopy("src/CNAME");
 
   // date filter: {{ "now" | date("yyyy") }} or {{ someDate | date("yyyy-LL-dd") }}
   eleventyConfig.addFilter("date", (value, format = "yyyy") => {
